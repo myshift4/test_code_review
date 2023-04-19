8cf65ab6b790cc9e97c28f046ee2d756fbc16ebe
xushutian <xushutian@topgamesinc.com>
2023-04-19 13:50:10 +0800

    update

--- python/test_openai.py
+++ python/test_openai.py
@@ -10,3 +10,6 @@ def get_ck_execute(run_sql):
     ck_conn = Client(host='192.168.119.136', port=9000, user='default', password='123456', database=app, send_receive_timeout=5)
     result = ck_conn.execute(run_sql)
     return result
+
+if __name__=="__main__":
+    print("test")
